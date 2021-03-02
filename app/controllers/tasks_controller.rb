f8ba79ce0fbb889c750f2ea867d67a0dc8c8c46d
Eula Gonzales <eulabgm@gmail.com>
2021-03-02 13:58:45 +0100

    Complete Rails Task Manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,97 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @tasks = Task.all
+    end
+
+    def show; end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+       @task = Task.new(task_params)
+       @task.save
+          
+       redirect_to task_path(@task)
+    end
+
+    def edit; end 
+
+    def update
+        @task.update(task_params)
+  
+        redirect_to task_path(@task)
+    end
+
+    
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+##########################################################################
+
+
+    private
+  
+     def task_params
+      params.require(:task).permit(:title, :details, :completed)
+    end
+  
+   def set_task
+     @task = Task.find(params[:id])
+   end
+
+
+end
+
+
+# class RestaurantsController < ApplicationController
+#     before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  
+#     def index
+#       @restaurants = Restaurant.all
+#     end
+  
+#     def show; end
+  
+#     def new
+#       @restaurant = Restaurant.new
+#     end
+  
+#     def create
+#       @restaurant = Restaurant.new(restaurant_params)
+#       @restaurant.save
+  
+#       redirect_to restaurant_path(@restaurant)
+#     end
+  
+#     def edit; end
+  
+#     def update
+#       @restaurant.update(restaurant_params)
+  
+#       redirect_to restaurant_path(@restaurant)
+#     end
+  
+#     def destroy
+#       @restaurant.destroy
+  
+#       redirect_to restaurants_path
+#     end
+  
+#     private
+  
+#     def restaurant_params
+#       params.require(:restaurant).permit(:name, :address, :rating)
+#     end
+  
+#     def set_restaurant
+#       @restaurant = Restaurant.find(params[:id])
+#     end
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,51 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html4
+   #  See all tasks 
+  get 'tasks', to: 'tasks#index'
+
+  # Show the restaurant form
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  # Create a restaurant
+  post "tasks", to: "tasks#create"
+
+   # See details about tasks
+  get "tasks/:id", to: "tasks#show", as: :task
+
+   # Update a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Destroy a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+
+
+# Rails.application.routes.draw do
+#   get 'tasks', to: 'tasks#index'
+#   get 'tasks/new', to: 'tasks#new', as: :new_task
+#   get 'tasks/:id', to: 'tasks#show', as: :task
+#   post 'tasks', to: 'tasks#create'
+#   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+#   patch 'tasks/:id', to: 'tasks#update'
+#   delete 'tasks/:id', to: 'tasks#destroy'
+# end
+
+ # # See all restaurants ✅
+  # get "restaurants", to: "restaurants#index"
+
+  # # Show the restaurant form
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # # Create a restaurant
+  # post "restaurants", to: "restaurants#create"
+
+  # # See details about one restaurant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # # Update a restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # Destroy a restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy"
